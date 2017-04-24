0f6c4a9882899cd75ef1eb73003b98c3623d98ff
manrodcor <mrcordones@gmail.com>
2017-04-24 10:13:04 +0200

    Correccion siguiendo consejos del profesor

--- main.c
+++ main.c
@@ -1,233 +1,135 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>//importa booleanos
+#include <stdbool.h>
 
 
-// TODO: Crea dos macros con el tamaño horizontal y vertical del mundo
+#define TAM_HOR 8
+#define TAM_VER 8
 
-#define TAM_HOR 10
-#define TAM_VER 10
-
-void world_init(bool *matriz);
-void world_print(bool *matriz);/* Recibo un mundo */
-void world_step(bool matriz01[TAM_VER][TAM_HOR], bool matriz02[TAM_VER][TAM_HOR]);// Recibo dos mundos 
-int world_count_neighbors(bool matriz[TAM_VER][TAM_HOR], int i, int j);/* Recibo un mundo y unas coordenadas */
-bool world_get_cell(bool matriz[TAM_VER][TAM_HOR], int i, int j);/* Recibo un mundo y unas coordenadas */
-void world_copy(bool matriz01[TAM_VER][TAM_HOR], bool matriz02[TAM_VER][TAM_HOR]);/* Recibo dos mundos */
-
-bool *pmundo01;
-bool *pmundo02;
+void world_init(bool matriz[TAM_VER][TAM_HOR]);
+void world_print(bool matriz[TAM_VER][TAM_HOR]);
+void world_step(bool matriz01[TAM_VER][TAM_HOR], bool matriz02[TAM_VER][TAM_HOR]);
+int world_count_neighbors(bool matriz[TAM_VER][TAM_HOR], int i, int j);
+bool world_get_cell(bool matriz[TAM_VER][TAM_HOR], int i, int j);
+void world_copy(bool matriz01[TAM_VER][TAM_HOR], bool matriz02[TAM_VER][TAM_HOR]);
 
 
 int main()
 {
 	int i = 0;
-	int j = 0;
-	// TODO: Declara dos mundos
+
+	//Declara dos mundos
 	bool mundo01[TAM_VER][TAM_HOR];
 	bool mundo02[TAM_VER][TAM_HOR];
 
-	
-	pmundo01 = *mundo01;
-	pmundo02 = *mundo02;
 
-	// TODO: inicializa el mundo
-	world_init(*mundo01);
-	world_init(*mundo02);
+	//Inicializa el mundo
+	world_init(mundo01);
+	world_init(mundo02);
 
 	do {
 		printf("\033cIteration %d\n", i++);
-		// TODO: Imprime el mundo
-		world_print(*mundo01);
-		//printf("\n");
-		//world_print(*mundo02);
-		// TODO: Itera
+		world_print(mundo01);
 		world_step(mundo01, mundo02);
 	} while (getchar() != 'q');
 
 	return EXIT_SUCCESS;
 }
 
-void world_init(bool *matriz)
+void world_init(bool matriz[TAM_VER][TAM_HOR])
 {
-	// TODO: Poner el mundo a false
+	//Poner el mundo a false
 	int i = 0;
 	int j = 0;
 	for(i = 0; i < TAM_VER; i++){
 		for(j = 0; j < TAM_HOR; j++){
-			*matriz = false;
-			*matriz++;
+			matriz[i][j] = false;
 		}
 	}
 
-	/* TODO: Inicializar con el patrón del glider:
+	/*Inicializar con el patrón del glider:
 	 *           . # .
 	 *           . . #
 	 *           # # #
 	 */
 
-	matriz = pmundo01;
-	for(i = 0; i < TAM_VER; i++){
-		for(j = 0; j < TAM_HOR; j++){
-			if((i == 0 && j == 1)||(i == 1 && j == 2)||(i == 2 && j == 0)||(i == 2 && j == 1)||(i == 2 && j == 2)){
-				*matriz = true;
-			}
-		*matriz++;
-		}
-	}
+	matriz[0][1] = true;
+	matriz[1][2] = true;
+	matriz[2][0] = true;
+	matriz[2][1] = true;
+	matriz[2][2] = true;
+	
 }
 
-void world_print(bool *matriz)
+void world_print(bool matriz[TAM_VER][TAM_HOR])
 {
-	// TODO: Imprimir el mundo por consola. Sugerencia:
-	/*
-	 *     . # . . . . . . . .
-	 *     . . # . . . . . . .
-	 *     # # # . . . . . . .
-	 *     . . . . . . . . . .
-	 *     . . . . . . . . . .
-	 *     . . . . . . . . . .
-	 *     . . . . . . . . . .
-	 *     . . . . . . . . . .
-	 *     . . . . . . . . . .
-	 *     . . . . . . . . . .
-	 */
 	int i = 0;
 	int j = 0;
 	for(i = 0; i < TAM_VER; i++){
 		for(j = 0; j < TAM_HOR; j++){
-			if(*matriz == false){
-				printf(". ");
-			}
-			else{
-				printf("# ");			
-			}
-			*matriz++;
+			printf("%s ", matriz[i][j] ? "#" : ".");
 		}
 		printf("\n");
 	}
 }
 
-void world_step(bool matriz01[TAM_VER][TAM_HOR], bool matriz02[TAM_VER][TAM_HOR])/* Recibo dos mundos */
+void world_step(bool matriz01[TAM_VER][TAM_HOR], bool matriz02[TAM_VER][TAM_HOR])
 {
-	/*
-	 * TODO:
-	 * - Recorrer el mundo célula por célula comprobando si nace, sobrevive
-	 *   o muere.
-	 *
-	 * - No se puede cambiar el estado del mundo a la vez que se recorre:
-	 *   Usar un mundo auxiliar para guardar el siguiente estado.
-	 *
-	 * - Copiar el mundo auxiliar sobre el mundo principal
-	 */
-
 	int i = 0;
 	int j = 0;	
-	int v;	//numero de vecinos
-	bool estado;//Estado de la celda
+	int nVecinos;
 
 	for(i = 0; i < TAM_VER; i++){
 		for(j = 0; j < TAM_HOR; j++){
-			v = world_count_neighbors(matriz01, i, j);
-			estado = world_get_cell(matriz01, i, j);
-			if((estado == false) & (v == 3)){
-				matriz02[i][j] = true;
-			}
-			else if((estado == true) & (v > 1) & (v < 4)){
-				matriz02[i][j] = false;			
-			}
-			else{
-				matriz02[i][j] = false;
-			}
+			nVecinos = world_count_neighbors(matriz01, i, j);
+			matriz02[i][j] = (matriz01[i][j] && nVecinos == 2) || nVecinos == 3;
 		}
 	}
 	world_copy(matriz01, matriz02);
 }
 
-int world_count_neighbors(bool matriz[TAM_VER][TAM_HOR], int i, int j)/* Recibo un mundo y unas coordenadas */
+int world_count_neighbors(bool matriz[TAM_VER][TAM_HOR], int i, int j)
 {
-	// Devuelve el número de vecinos
 	int numeroVecinos = 0;
-	int iaux = i - 1;
-	int jaux = j - 1;
-	int aux01 = i + 1;
-	int aux02 = j + 1;
-	if(i == 0 || j == 0 || i == TAM_VER - 1 || j == TAM_HOR - 1){//Estamos en la periferia del mundo
-		if(i == 0)
-			iaux = TAM_VER - 1;
-		if(i == TAM_VER - 1){
-			iaux = 0;
-			aux01 = i - 1;
-			}
-		if(j == 0)
-			jaux = TAM_HOR - 1;
-		if(j == TAM_HOR - 1){
-			jaux = 0;
-			aux02 = j - 1;
-			}
-
-		if(matriz[i][jaux])
-			numeroVecinos++;
-		if(matriz[iaux][jaux])
-			numeroVecinos++;
-		if(matriz[iaux][j])
-			numeroVecinos++;
-		if(matriz[iaux][aux02])
-			numeroVecinos++;
-		if(matriz[i][aux02])
-			numeroVecinos++;
-		if(matriz[aux01][aux02])
-			numeroVecinos++;
-		if(matriz[aux01][j])
-			numeroVecinos++;
-		if(matriz[aux01][jaux])
-			numeroVecinos++;
-	}
-	else{
-		if(matriz[i - 1][j - 1])
-			numeroVecinos++;
-		if(matriz[i - 1][j])
-			numeroVecinos++;
-		if(matriz[i - 1][j + 1])
-			numeroVecinos++;
-		if(matriz[i][j - 1])
-			numeroVecinos++;
-		if(matriz[i][j + 1])
-			numeroVecinos++;
-		if(matriz[i + 1][j - 1])
-			numeroVecinos++;
-		if(matriz[i + 1][j])
-			numeroVecinos++;
-		if(matriz[i + 1][j + 1])
-			numeroVecinos++;
-	}
+	
+	numeroVecinos += world_get_cell(matriz, i, j - 1);
+	numeroVecinos += world_get_cell(matriz, i - 1, j - 1);
+	numeroVecinos += world_get_cell(matriz, i - 1, j);
+	numeroVecinos += world_get_cell(matriz, i - 1, j + 1);
+	numeroVecinos += world_get_cell(matriz, i, j + 1);
+	numeroVecinos += world_get_cell(matriz, i + 1, j + 1);
+	numeroVecinos += world_get_cell(matriz, i + 1, j);
+	numeroVecinos += world_get_cell(matriz, i + 1, j - 1);
+	
 	return numeroVecinos;
 }
 
-bool world_get_cell(bool matriz[TAM_VER][TAM_HOR], int i, int j)/* Recibo un mundo y unas coordenadas */
+bool world_get_cell(bool matriz[TAM_VER][TAM_HOR], int i, int j)
 {
-	/*
-	 * TODO: Devuelve el estado de la célula de posición indicada
-	 * (¡cuidado con los límites del array!)
-	 */
-	bool estado = matriz[i][j];
+	bool estado;
+	if(i == -1){
+		i = TAM_VER -1;
+	}
+	else if(i == TAM_VER){
+		i = 0;
+	}
+	if(j == -1){
+		j = TAM_VER -1;
+	}
+	else if(j == TAM_VER){
+		j = 0;
+	}
+	estado = matriz[i][j];
 	return estado;
 }
 
-void world_copy(bool matriz01[TAM_VER][TAM_HOR], bool matriz02[TAM_VER][TAM_HOR])/* Recibo dos mundos */
+void world_copy(bool matriz01[TAM_VER][TAM_HOR], bool matriz02[TAM_VER][TAM_HOR])
 {
-	// TODO: copia el mundo segundo mundo sobre el primero
-
 	int i,j;
 
 	for(i = 0; i < TAM_VER; i++){
 		for(j = 0; j < TAM_HOR; j++){
-			if(matriz02[i][j] == true){
-				matriz01[i][j] = true;
-			}
-			else{
-				matriz01[i][j] = false;
-			}	
+			matriz01[i][j] = matriz02[i][j];	
 		}
 	}
 
